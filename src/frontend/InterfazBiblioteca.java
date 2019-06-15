f63a2c8f46a2e0903e166148d3ffbf38716f9a94
Lele <nilocuru@hotmail.com>
2019-06-14 19:05:30 -0500

    Commit inicial. Se agrega paquete y clase principal de Front End

---
+++ src/frontend/InterfazBiblioteca.java
@@ -0,0 +1,42 @@
+/**
+ * Clase principal de la biblioteca.
+ */
+package frontend;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Nicolás Londoño
+ */
+public class InterfazBiblioteca extends JFrame 
+{
+	//----------------------------------------------------------------------------------
+		//Atributos y Constantes
+	//----------------------------------------------------------------------------------
+	
+	/**
+	 * Constante de serialización
+	 */
+	private static final long serialVersionUID = 1L;
+
+	//----------------------------------------------------------------------------------
+		//Métodos
+	//----------------------------------------------------------------------------------
+	/*
+	 * Método constructor de la interfáz
+	 */
+	public InterfazBiblioteca()
+	{
+		
+	}
+	/**
+	 * Método principal de la aplicación
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
